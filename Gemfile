9b323a92ac70e83abe7c45af328994b174c1ee61
julien garcia <ju.garcia38@yahoo.fr>
2020-12-01 09:35:21 +0100

    V1 active-menu

--- Gemfile
+++ Gemfile
@@ -21,6 +21,7 @@ gem 'jbuilder', '~> 2.7'
 # gem 'redis', '~> 4.0'
 # Use Active Model has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
+gem 'active_link_to'
 
 # Use Active Storage variant
 # gem 'image_processing', '~> 1.2'
@@ -65,4 +66,3 @@ gem 'devise'
 gem "redis", "~> 4.2"
 
 gem "amazing_print", "~> 1.2"
-gem 'active_link_to'
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,13 @@
 module ApplicationHelper
   def current_class?(root_path)
-    return 'active' if request.path == root_path
-    ''
+    if request.path == root_path
+      return 'active'
+    elsif request.path == barcode_path
+      return 'active'
+    elsif request.path == products_path
+      return 'active'
+    else
+      return 'active'
+    end
   end
 end
