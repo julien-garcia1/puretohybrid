64d03f4b81014995554981c5a5d6614282d189b9
julien garcia <ju.garcia38@yahoo.fr>
2021-01-15 15:47:28 +0100

    New status added

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   # resources :store, only: [:show] do
   resources :tickets, only: [:show, :index, :create, :new] do
     get :assign, to: "tickets#assign", on: :member
+    get :assign_later, to: "tickets#assign_later", on: :member
     get :closed, to: "tickets#closed", on: :member
     resources :reviews, only: [:new, :create, :show ]
   end
